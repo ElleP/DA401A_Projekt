7d3b1f1e5eda0d58bfe09640c51f353b01159bd7
Elle <michelle.pantorp@bredband.net>
2015-10-17 11:56:52 +0200

    Small changes in css

--- public_screen/question.php
+++ public_screen/question.php
@@ -2,26 +2,21 @@
 
 <!DOCTYPE html>
 
+<!--Include of header-file-->
 <?php include('header.php');?>
 
-	<div class="container-fluid">
-
-		<div id="row">
-
-			<div class="col-md-6 col-md-offset-3">
-				<div class="box">
-					<h2>Create question</h2>
-					<form method="" action="">
-						<input type="text" name="question_input" id="question_input" placeholder="Type question here"></input>
-						<button>Save question</button>
-					</form>
-				</div><!--End of .box-->
-
-
-			</div><!--End of .col-->
-
-		</div><!--End of .row-->
-
-	</div><!--End of wrapper-->
-</body>
+		<div class="container-fluid">
+			<div id="row">
+				<div class="col-md-6 col-md-offset-3">
+					<div class="box">
+						<h2>Create question</h2>
+						<form method="" action="">
+							<input type="text" name="question_input" id="question_input" placeholder="Type question here"></input>
+							<button>Save question</button>
+						</form>
+					</div><!--End of .box-->
+				</div><!--End of .col-->
+			</div><!--End of .row-->
+		</div><!--End of wrapper-->
+	</body>
 </html>
