af5f9d51761d3ba7d064332babf8d17394a701c4
Elle <michelle.pantorp@bredband.net>
2015-10-17 11:31:44 +0200

    Made form for input of question

--- public_screen/header.php
+++ public_screen/header.php
@@ -2,6 +2,16 @@
 <head>
 	<title>Instantify</title>
 	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+	<!--jQuery-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+	<!--Bootstrap-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+
+
 </head>
 <body>
 	<header>
--- public_screen/question.php
+++ public_screen/question.php
@@ -8,7 +8,15 @@
 
 		<div id="row">
 
-			<div class="col-md-4 col-md-offset-2">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="box">
+					<h2>Create question</h2>
+					<form method="" action="">
+						<input type="text" name="question_input" id="question_input" placeholder="Type question here"></input>
+						<button>Save question</button>
+					</form>
+				</div><!--End of .box-->
+
 
 			</div><!--End of .col-->
 
