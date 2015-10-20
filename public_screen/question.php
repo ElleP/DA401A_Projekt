c0a60573173a611126ee92715a41bc7e394fa69a
Julian <j.lee.blair@gmail.com>
2015-10-20 10:35:04 +0200

    Did lots of stuffs
    added js file
    added angular elements in question.php

--- public_screen/question.php
+++ public_screen/question.php
@@ -1,6 +1,7 @@
 <!--HTMLfile for Instantify questions screen-->
 
 <!DOCTYPE html>
+<html ng-app="app">
 
 <!--Include of header-file-->
 <?php include('header.php');?>
@@ -8,11 +9,12 @@
 		<div class="container-fluid">
 			<div id="row">
 				<div class="col-md-6 col-md-offset-3">
-					<div class="box">
-						<h2>Create question</h2>
+					<div class="box" ng-controller="MessageController">
+						<h2>{{msgCreate}}</h2>
 						<form method="" action="">
-							<input type="text" name="question_input" id="question_input" placeholder="Type question here"></input>
-							<button>Save question</button>
+                             <!--End of .boxtype="text" name="question_input" id="question_input" placeholder="Type question here"-->
+							<input>{{msgInput}}</input>
+							<button>{{btnSave}}</button>
 						</form>
 					</div><!--End of .box-->
 				</div><!--End of .col-->
