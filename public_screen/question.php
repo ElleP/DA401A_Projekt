dd6b2df16a409f19fdd0516405ac09ac19b5a1f8
Julian <j.lee.blair@gmail.com>
2015-10-20 12:18:59 +0200

    Added basic firebase set method
    did this

--- public_screen/question.php
+++ public_screen/question.php
@@ -9,10 +9,9 @@
 				<div class="col-md-6 col-md-offset-3">
 					<div ng-controller="MessageController" class="box">
 						<h2>{{msgCreate}}</h2>
-						<form>
-							<input ng-model="courseID" type="text" placeholder={{msgID}}></input>
+						<form method="" action="">
 							<input ng-model="question" type="text" placeholder={{msgInput}}></input>
-							<input type="button" ng-click="submitData(courseID, question)" value={{btnSave}}></input>
+							<input type="button" ng-click="submitData()" value={{btnSave}}></input>
 						</form>
 					</div><!--End of .box-->
 				</div><!--End of .col-->
